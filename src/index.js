5b39204da346399b5b46cb2db4248566570acd33
Wendy Beck <wbeck@simplehealth.com>
2022-04-20 16:39:29 -0700

    debugging heroku deploy

--- src/index.js
+++ src/index.js
@@ -14,15 +14,12 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server
-    .listen({ port: process.env.PORT || 4000 })
-    .then((url) => {
-      console.log(`
-        🚀  Server is running
-        🔉  Listening on port ${port}
-        📭  Query at ${url}
-      `);
-    });
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
 }
 
 startApolloServer(typeDefs, resolvers);
